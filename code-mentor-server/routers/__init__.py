89cddede28296a6fc31cbc18119c2c500315ee05
chutibaba <nadeeshayashodara@gmail.com>
2025-10-23 16:02:03 +0530

    Fix Reset_Password import to match file name

--- code-mentor-server/routers/__init__.py
+++ code-mentor-server/routers/__init__.py
@@ -17,7 +17,7 @@ from .assignment_list import router as assignment_list_router
 from .assignment_details import router as assignment_details_router
 from .code_submit import router as code_submit_router
 from .assignment import router as assignment_router
-from .Reset_Password import router as reset_password_router
+from .reset_password import router as reset_password_router
 from .report import router as report_router
 from .student import router as student_router
 from .instructor import router as instructor_router
